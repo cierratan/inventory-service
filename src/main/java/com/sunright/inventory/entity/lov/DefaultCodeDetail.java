d3ef9c1d4ab8dbd2a579a0398420bc822a9df144
Teguh Wanrasagita <teguh@dedomena.com.sg>
2021-12-28 21:21:48 +0800

    fix lombok warning

--- src/main/java/com/sunright/inventory/entity/lov/DefaultCodeDetail.java
+++ src/main/java/com/sunright/inventory/entity/lov/DefaultCodeDetail.java
@@ -2,6 +2,7 @@ package com.sunright.inventory.entity.lov;
 
 import com.sunright.inventory.entity.BaseEntity;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.EmbeddedId;
@@ -11,6 +12,7 @@ import javax.persistence.Version;
 @Entity(name = "DEFAULT_CODE_DET")
 @Data
 @NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 public class DefaultCodeDetail extends BaseEntity {
     @EmbeddedId
     private DefaultCodeDetailId id;
